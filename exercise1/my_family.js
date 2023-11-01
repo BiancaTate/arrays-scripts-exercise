bde6d61e97008b72e19b56e0e73786e00c433120
BiancaTate <tatebianca1204@gmail.com>
2023-11-01 10:10:24 -0400

    "Exercise 1: Loop through array with 4 names"

---
+++ exercise1/my_family.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let names= ["John", "James", "Susan", "Jane"]
+
+console.log(names)
+
+
+// Defining     // Checking             //Changing
+for(let index= 0; index < names.length; index += 1 ){
+    console.log(names[index])
+}
+
+console.log("Loop is complete")
