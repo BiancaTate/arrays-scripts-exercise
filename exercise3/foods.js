f853a4563d17b334168dcfeaa649d17699665ecc
BiancaTate <tatebianca1204@gmail.com>
2023-11-01 15:30:28 -0400

    "Exercise 3"

---
+++ exercise3/foods.js
@@ -0,0 +1,50 @@
+"use strict"
+
+// Array of objects 
+// var objArr = [ 
+//     { 
+//         name: 'john', 
+//         age: 12, 
+//         gender: 'male'
+//     }, 
+//     { 
+//         name: 'jane', 
+//         age: 15, 
+//         gender: 'female'
+//     }, 
+//     { 
+//         name: 'julie', 
+//         age: 20, 
+//         gender: 'trans'
+//     } 
+// ]; 
+
+let mealSum = 0
+let tax = .08
+let tip = .18
+
+let meals = [
+    { item: "Butter Chicken", price: 16.50 },
+    { item: "Garlic Pratha", price: 6.25, },
+    { item: "Mango Laasi", price: 3.75, }
+]
+
+
+for (let index = 0; index < meals.length; index += 1) {
+
+    mealSum = mealSum + meals[index].price
+
+}
+
+let subTotal = mealSum
+let subTax = subTotal * tax
+let subTip= subTotal * tip
+let totalDue = subTotal + subTax + subTip
+
+console.log("The subtotal = $" +subTotal)
+console.log("The subtotal with tax = $" +subTax)
+console.log("The subtotal with tip = $" +subTip)
+console.log("The Total Amount = $" +totalDue)
+
+
+
